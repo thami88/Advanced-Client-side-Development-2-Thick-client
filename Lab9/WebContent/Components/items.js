941f0aa8b484c589255230b047b8222e5c589a24
thami <thami@DESKTOP-LQJ1807>
2020-05-01 13:16:26 +0530

    Implement the Update button handler

--- Lab9/WebContent/Components/items.js
+++ Lab9/WebContent/Components/items.js
@@ -22,3 +22,16 @@ $(document).on("click", "#btnSave", function(event) {
 	// If valid------------------------
 	$("#formItem").submit();
 });
+
+// UPDATE==========================================
+$(document).on(
+		"click",
+		".btnUpdate",
+		function(event) {
+			$("#hidItemIDSave").val(
+					$(this).closest("tr").find('#hidItemIDUpdate').val());
+			$("#itemCode").val($(this).closest("tr").find('td:eq(0)').text());
+			$("#itemName").val($(this).closest("tr").find('td:eq(1)').text());
+			$("#itemPrice").val($(this).closest("tr").find('td:eq(2)').text());
+			$("#itemDesc").val($(this).closest("tr").find('td:eq(3)').text());
+		});
