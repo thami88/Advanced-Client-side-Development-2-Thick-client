78d3066494bcf34968d955a1cc9238350b8a5e40
thami <thami@DESKTOP-LQJ1807>
2020-05-01 13:14:56 +0530

    Implement Save button click handler

--- Lab9/WebContent/Components/items.js
+++ Lab9/WebContent/Components/items.js
@@ -4,3 +4,21 @@ $(document).ready(function() {
 	}
 	$("#alertError").hide();
 });
+
+// SAVE ============================================
+$(document).on("click", "#btnSave", function(event) {
+	// Clear alerts---------------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+	// Form validation-------------------
+	var status = validateItemForm();
+	if (status != true) {
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+	// If valid------------------------
+	$("#formItem").submit();
+});
